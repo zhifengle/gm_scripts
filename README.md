dab757796a8d7472139ef9312490e0aa8d8d485d
22earth <kidl123@163.com>
2015-08-24 00:10:17 +0800

    add a feature to auto click in baiducloud page for off-line download

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # UserScripts
+
 > 自用油猴脚本和userChromejs
 
+
 ## bilidan.uc.js
-    向火狐添加右键菜单，调用bilidan.py 和MPV播放bilibili.com视频
+向火狐添加右键菜单，调用bilidan.py 和MPV播放bilibili.com视频
 ## bt_search_for_bgm.user.js
-    bangumi辅助搜索
-    为bangumi界面添加bt搜索图标
-    方便在查阅条目的时候，搜索资源
-默认开启三个搜索引擎： [dmhy](https://share.dmhy.org/ "dmhy") [Download Search](http://search.jayxon.com/ "google") [btcherry](http://www.btcherry.net "btcherry")
+bangumi辅助搜索
+为bangumi界面添加bt搜索图标
+方便在查阅条目的时候，搜索资源
+默认开启三个搜索引擎： [dmhy](https://share.dmhy.org/ "dmhy") [Download Search](http://search.jayxon.com/ "google") [cilizhushou](http://www.cilizhushou.com/ "cilizhushou")
 可以根据需要添加搜索引擎
 或者注释searchEngineLists里相关项
+
+## fixed_magnet_for_baiducloud.user.js
+修改dmhy（花园），popgo（漫游）磁力链接以便于百度云离线
+
+点击Magnet链接后面的文字：“百度云”，可以自动辅助点击离线下载。
--- fixed_magnet_for_baiducloud.user.js
+++ fixed_magnet_for_baiducloud.user.js
@@ -3,17 +3,108 @@
 // @namespace   https://github.com/22earth
 // @include     https://share.dmhy.org/topics/view/*
 // @include     http://share.popgo.org/program*
-// @version     0.1
-// @grant       none
+// @include     http://pan.baidu.com/disk/home
+// @version     0.2
+// @grant       GM_addStyle
+// @grant       GM_setValue
+// @grant       GM_getValue
 // ==/UserScript==
 
-if (window.location.href.match("popgo") && document.getElementById("si_downseed")) {
-    var torrent_hash = document.getElementById("si_downseed").children[0].href.split("=")[1];
-    var magnet_node = document.getElementById("si_magnet").children[0];
-    magnet_node.href = "magnet:?xt=urn:btih:" + torrent_hash;
-}    
-if (window.location.href.match("dmhy") && document.getElementById("tabs-1")) {
-    var torrent_hash = document.getElementById('tabs-1').children[0].children[1].pathname.replace(/\/(.*)\/(.*)\.torrent/, "$2");
-    var magnet_node = document.getElementById("a_magnet");
-    magnet_node.href = "magnet:?xt=urn:btih:" + torrent_hash;
-}
+(function () {
+  var btsite = {
+    init: function () {
+      this.addSearchAnchor(this.fixMagnet());
+    },
+
+    fixMagnet: function () {
+      var torrentHash = null, magnetNode = null;
+      if (window.location.href.match("popgo") && document.getElementById("si_downseed")) {
+        torrentHash = document.getElementById("si_downseed").children[0].href.split("=")[1];
+        magnetNode = document.getElementById("si_magnet").children[0];
+        GM_setValue('magneturl', "magnet:?xt=urn:btih:" + torrentHash);
+
+      }    
+      if (window.location.href.match("dmhy") && document.getElementById("tabs-1")) {
+        torrentHash = document.getElementById('tabs-1').children[0].children[1].pathname.replace(/\/(.*)\/(.*)\.torrent/, "$2");
+        magnetNode = document.getElementById("a_magnet");
+        GM_setValue('magneturl', "magnet:?xt=urn:btih:" + torrentHash);
+      }
+      console.log(GM_getValue("magneturl"));
+      return magnetNode;
+    },
+
+    addSearchAnchor: function (aNode) {
+      if (aNode) {
+        // todo: create a function to make the anchor.
+        var frag = document.createDocumentFragment();
+        var anchor = document.createElement("a");
+        anchor.className = "magnet-download";
+        anchor.textContent = "百度云";
+        anchor.href = "http://pan.baidu.com/disk/home";
+        anchor.target = "_blank";
+        // add another anchor for magnet.
+        var magnetAnchor = document.createElement("a");
+        magnetAnchor.textContent =  "磁力链接";
+        magnetAnchor.className = "magnet-download";
+        magnetAnchor.href = GM_getValue("magneturl");
+        this.addStyle();
+        frag.appendChild(anchor);
+        frag.appendChild(magnetAnchor);
+        aNode.parentNode.appendChild(frag);
+        console.log("creat a anchor");
+      }
+    },
+
+    addStyle: function (css) {
+      if (css) {
+        GM_addStyle(css);
+      }
+      else {
+        GM_addStyle([
+          '.magnet-download{color: rgb(0, 180, 30) !important;margin-left: 4px;}',
+          '.magnet-download:hover{color:red !important;}',
+        ].join(''));
+      }
+    },
+  };
+
+
+  var bdcloud = {
+    init: function () {
+      var magneturl = GM_getValue('magneturl');
+      console.log(magneturl);
+      GM_setValue('magneturl', '');
+      if (magneturl) {
+        var scriptnode = document.createElement("script");
+        scriptnode.innerHTML = '(' + this.addToOffline.toString() + ')(\'' + magneturl + '\');';
+        document.body.appendChild(scriptnode);
+      }
+    },
+
+    addToOffline: function (magneturl) {
+      $(".icon-btn-download").click();
+      setTimeout(function () {
+        $("#_disk_id_13").click();
+        setTimeout(function () {
+          if (!($('#share-offline-link')[0].value) && window.stop) {
+            window.stop();
+            $('#share-offline-link')[0].value = magneturl;
+          }
+          $("#_disk_id_17").click();
+        }, 500);
+      }, 500);
+    },
+  };
+
+  if (window.location.hostname.match(/dmhy|popgo/)) {
+    btsite.init();
+  }
+  if (window.location.hostname.match("baidu")) {
+    bdcloud.init();
+  }
+
+})();
+
+
+
+

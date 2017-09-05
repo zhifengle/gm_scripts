ab716dac8a06471232e0f3e1dc343e242c5fe43d
22earth <zhifengle@gmail.com>
2017-09-05 13:52:05 +0800

    Add UA

--- qiandao_2dkf.js
+++ qiandao_2dkf.js
@@ -1,16 +1,33 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+  pageSettings: {
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
+  },
+ viewportSize: {
+    width: 1920,
+    height: 631
+  }
+});
 casper.start('http://bbs.2dkf.com/login.php', function() {
+  //this.waitForSelector('tr>td>input[name="pwuser"]');
   this.echo(this.getTitle());
-  
 });
 casper.then(function() {
-  this.sendKeys('tr>td>input[name="pwuser"]', casper.cli.get(0));
-  this.sendKeys('tr>td>input[name="pwpwd"]', casper.cli.get(1));
-  this.click('input.indlogin[type="submit"]');
+  var f = this.evaluate(function() {
+    return document.body.innerHTML;
+  });
+  this.echo(f);
 });
-casper.wait(3000);
-casper.thenOpen('http://bbs.2dkf.com/kf_growup.php', function() {
-  this.click('table div>a[href^="kf_growup"]');
-});
+/*
+ *casper.then(function() {
+ *  this.sendKeys('tr>td>input[name="pwuser"]', casper.cli.get(0));
+ *  this.sendKeys('tr>td>input[name="pwpwd"]', casper.cli.get(1));
+ *  this.click('input.indlogin[type="submit"]');
+ *});
+ */
+/*
+ *casper.wait(3000);
+ *casper.thenOpen('http://bbs.2dkf.com/kf_growup.php', function() {
+ *  this.click('table div>a[href^="kf_growup"]');
+ *});
+ */
 casper.run();
-
--- qiandao_v2ex.js
+++ qiandao_v2ex.js
@@ -1,4 +1,12 @@
-var casper = require('casper').create();
+var casper = require('casper').create({
+  pageSettings: {
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36'
+  },
+ viewportSize: {
+    width: 1920,
+    height: 631
+  }
+});
 casper.start('https://www.v2ex.com/signin', function() {
   this.echo(this.getTitle());
 });
@@ -15,6 +23,11 @@ casper.then(function() {
 });
 casper.wait(3000);
 casper.thenOpen('https://www.v2ex.com/mission/daily', function() {
+  if(this.evaluate(isMissionCompleted)) return;
   this.click('.cell>input');
 });
+function isMissionCompleted() {
+  var btn = document.querySelector('.cell>input');
+  return btn.value === '查看我的账户余额';
+}
 casper.run();

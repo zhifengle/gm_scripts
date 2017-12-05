b3e2c9653767ce87cf0ecaedd0a83f10af6b58ec
22earth <zhifengle@gmail.com>
2017-12-05 20:30:09 +0800

    Update webpack

--- bangumi-new-wiki-helper/webpack.config.js
+++ bangumi-new-wiki-helper/webpack.config.js
@@ -15,37 +15,13 @@ module.exports = {
   output: {
     // This copies each source entry into the extension dist folder named
     // after its entry config key.
-    path: 'extension/dist',
+    path: path.join(__dirname, 'extension/dist'),
     filename: '[name].js',
   },
   module: {
-    // This transpiles all code (except for third party modules) using Babel.
-    loaders: [{
-      exclude: /node_modules/,
-      test: /\.js$/,
-      // Babel options are in .babelrc
-      loaders: ['babel'],
-    }],
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+    ]
   },
-  resolve: {
-    // This allows you to import modules just like you would in a NodeJS app.
-    extensions: ['', '.js', '.jsx'],
-    root: [
-      path.resolve(__dirname),
-    ],
-    modulesDirectories: [
-      'src',
-      'node_modules',
-    ],
-  },
-  plugins: [
-    // Since some NodeJS modules expect to be running in Node, it is helpful
-    // to set this environment var to avoid reference errors.
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-  ],
-  // This will expose source map files so that errors will point to your
-  // original source files instead of the transpiled files.
   devtool: 'sourcemap',
 };

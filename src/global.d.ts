7dda3607fdb6d7625d6f89f50a3683bd8a9fbabe
22earth <zhifengle@gmail.com>
2021-06-24 16:52:30 +0800

    update: gm types

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,11 +1,25 @@
 declare var Fuse: any;
 
-declare var GM_setValue: any;
-declare var GM_getValue: any;
-declare var GM_registerMenuCommand: any;
-declare var GM_addStyle: any;
+declare var GM_setValue: (key: string, value: any) => void;
+declare var GM_getValue: (key: string, defaultValue?: any) => any;
+declare var GM_listValues: () => string[];
+declare var GM_deleteValue: (key: string) => void;
+declare var GM_registerMenuCommand: (
+  caption: string,
+  onClick: Function,
+  shortcut?: string
+) => void;
+declare var GM_addStyle: (style: string) => void;
 declare var GM_openInTab: any;
-declare var GM_getResourceText: any;
+declare var GM_getResourceText: (url: string) => string;
+declare var GM_log: (url: string) => void;
+declare var GM_notification: (options: {
+  text: string;
+  title?: string;
+  image?: string;
+  onClick: Function;
+  onDone: Function;
+}) => any;
 
 // declare var bangumiDataURL: string;
 
